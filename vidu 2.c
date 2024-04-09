cb9caab3b8099d2ba4fc02219a2612f4d055e669
TrDungxx <Hovercard27@gmail.com>
2024-04-09 11:16:56 +0700

    vidu2

---
+++ vidu 2.c
@@ -0,0 +1,24 @@
+//
+// Created by hover on 4/9/2024.
+//
+#include <stdio.h>
+ int main (){
+ int a ,b ,c ,h;
+    printf ("diem toan:");
+    scanf ("%d",&a);
+     printf("diem van:");
+     scanf("%d",&b);
+     printf("diem anh:");
+     scanf("%d", &c);
+     h=(a+b)*2 +c;
+     printf("tong diem la: %d",h);
+     if
+     (h>25 && h<=45)
+     {printf("ban da do\n");
+     }
+     else {
+         printf("ban da truot");
+     }
+     return 0;
+
+}
